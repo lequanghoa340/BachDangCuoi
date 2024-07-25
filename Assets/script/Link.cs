522f8ea77b36e4c20276e84239642fd950722ea5
lequanghoa340 <125871175+lequanghoa340@users.noreply.github.com>
2024-07-25 23:37:58 +0700

    HyperLink

---
+++ Assets/script/Link.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public void OpenYouWed()
+    {
+        Application.OpenURL("https://bachdangsu.site/");
+    }
+}
