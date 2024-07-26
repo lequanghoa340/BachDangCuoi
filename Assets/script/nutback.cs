f79ad24a219890e4c4222ac33277333e3e9504f6
luanhuupuannn <luanhuupuan@gmail.com>
2024-07-26 17:17:50 +0700

    fixx

---
+++ Assets/script/nutback.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nutback : MonoBehaviour
+{
+
+    public GameObject tat;
+   
+    public void back()
+    {
+        tat.SetActive(false);
+    }
+}
